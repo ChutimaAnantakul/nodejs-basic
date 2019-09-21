9368e646edc69cca827e7712b7ff8a490843c2dc
ChutimaAnantakul <Chutima.an@mail.wu.ac.th>
2019-09-21 12:22:19 +0700

    add if-for&function in readme

---
+++ 2-if-for.js
@@ -0,0 +1,42 @@
+// const a = "a";
+// if (a === "a") {
+//   console.log("It's a");
+// } else {
+//   console.log("It's not a");
+// }
+
+// for (var i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+// console.log(i);
+
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// let sum = 0;
+// while (let i = 1; i <= 1000;i++) {
+//   sum+= 1;
+// }
+// console.log(sum);
+
+let a = 1; // int a = 1;
+let b = 1.1; // float b = 1.1;
+let c = "string"; // string c = "hello"
+let d = true; // bool d = true
+let e = false; // bool e = false
+let f = ["a", "b", "c"]; // string *f = ["a", "b", "c"]
+f[0] = "f";
+f.push("h");
+console.log(f);
+let h = [];
+let student = {
+  id: "61111415",
+  "name-lastname": "Chutima",
+  Faculty: "Software"
+};
+console.log(student["name-lastname"]);
+student["name-lastname"] = "Chu";
+console.log(student["name-lastname"]);
